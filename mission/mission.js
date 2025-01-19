7fc4faede7a8a1279e6e5ff488e3f1c94615190c
JonnyAcosta <114012848+JonnyAcosta@users.noreply.github.com>
2025-01-18 22:35:20 -0700

    added dark theme

---
+++ mission/mission.js
@@ -0,0 +1,15 @@
+const themeSelector = document.querySelector('#theme-select');
+function changeTheme(){
+    const body = document.body;
+    const logo = document.querySelector('.logo');
+
+    if (themeSelector.value === 'dark'){
+        body.classList.add('dark');
+        logo.src = 'byui-logo_white.webp';
+    } else {
+        body.classList.remove('dark');
+        logo.src = 'byui-logo_blue.webp';
+    }
+}
+
+themeSelector.addEventListener('change', changeTheme);
